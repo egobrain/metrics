0a9de3e873f6e314eff61cfbd572e1f98bd11aa8
Kozlov Yakov <yk@exante.eu>
2018-08-21 00:33:08 +0300

    Reduce queue memory usage

--- src/metrics_queue.erl
+++ src/metrics_queue.erl
@@ -30,11 +30,15 @@ new(TimeSpan) -> #window{time_span=TimeSpan}.
 
 -spec push(value(), timestamp(), window()) -> window().
 push(V, Ts, #window{buff=[]}=W) -> W#window{start_ts=Ts, buff=[{Ts,V}]};
-push(V, Ts, #window{time_span=TimeSpan, start_ts = StartTs, buff = Buff}=W) ->
-    case Ts - StartTs > TimeSpan of
-        true -> W#window{start_ts=Ts, rev_buff=Buff, buff=[{Ts, V}]};
-        false -> W#window{buff=[{Ts, V}|Buff]}
+push(V, Ts, #window{time_span=TimeSpan, start_ts = StartTs, buff = Buff, rev_buff=RevBuff}=W) ->
+    MinTs = Ts - TimeSpan,
+    case StartTs < MinTs of
+        true -> W#window{start_ts=Ts, rev_buff=clear(lists:reverse(Buff), MinTs), buff=[{Ts, V}]};
+        false -> W#window{buff=[{Ts, V}|Buff], rev_buff=clear(RevBuff, MinTs)}
     end.
+
+clear([{Ts, _V}|Rest], MinTs) when Ts < MinTs -> clear(Rest, MinTs);
+clear(Buff, _MinTs) -> Buff.
 
 -spec foldl(timestamp(), Fun, Acc, window()) -> Acc when
       Fun :: fun((value(), Acc) -> Acc).
@@ -45,9 +49,11 @@ foldl(Ts, Fun, Acc, #window{time_span=TimeSpan, buff=Buff, rev_buff=RevBuff}) ->
             (_, A) -> A
         end, foldr_(Fun, Acc, MinTs, RevBuff), Buff).
 
-foldr_(Fun, Acc, MinTs, [{Ts, V}|Rest]) when Ts >= MinTs ->
+foldr_(Fun, Acc, MinTs, [{Ts, _V}|Rest]) when Ts < MinTs ->
+    foldr_(Fun, Acc, MinTs, Rest);
+foldr_(Fun, Acc, MinTs, [{_Ts, V}|Rest]) ->
     foldr_(Fun, Fun(V, Acc), MinTs, Rest);
-foldr_(_Fun, Acc, _MinTs, _) -> Acc.
+foldr_(_Fun, Acc, _MinTs, []) ->  Acc.
 
 %% =============================================================================
 %% Tests
@@ -65,6 +71,9 @@ window_test() ->
     WR = lists:foldl(fun(I, W) -> ?MODULE:push(I*10, I, W) end, W1, lists:seq(1, 10)),
     ?assertEqual(
         [100,90,80,70],
-        ?MODULE:foldl(12, fun(I, Acc) -> [I|Acc] end, [], WR)).
+         ?MODULE:foldl(12, fun(I, Acc) -> [I|Acc] end, [], WR)),
+    ?assertEqual(
+        [100,90,80,70,60,50],
+        ?MODULE:foldl(9, fun(I, Acc) -> [I|Acc] end, [], WR)).
 
 -endif.
